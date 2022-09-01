31fab13b42959ae807d2971e5e3c0e0a0b8a0047
hadesyi <leesk55@gmail.com>
2022-09-01 16:10:33 +0900

    3. numpy의 다차원 배열 3-1 다차원 배열이란?

---
+++ pandas/ex2.py
@@ -0,0 +1,18 @@
+# numpy 패키지 불러오기 및 np라는 약어로 지칭하기
+import numpy as np
+
+# 스칼라
+a0 = np.array(1)
+print(a0)
+
+# 벡터
+a1 = np.array([1, 2])
+print(a1)
+
+# 행렬
+a2 = np.array([[1, 2], [3, 4]])
+print(a2)
+
+# 텐서
+a3 = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 4, 8]]])
+print(a3)
