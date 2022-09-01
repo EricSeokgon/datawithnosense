c2d262c3b67b9a4143d0ed14bced0c7cf508468c
hadesyi <leesk55@gmail.com>
2022-09-01 16:14:19 +0900

    3. numpy의 다차원 배열 3-2 다차원 배열 다루기

--- pandas/ex2.py
+++ pandas/ex2.py
@@ -23,4 +23,14 @@ print(a0.ndim, a1.ndim, a2.ndim, a3.ndim)
 # shape 속성을 애용해 배열의 크기 확인
 print(a0.shape, a1.shape, a2.shape, a3.shape)
 
+# 3차원 배열에서 1번쨰 행렬 필터링
+print(a3[0])
+
+# 3차원 배열에서 1번째 행렬의 2번째 행 필터링
+print(a3[0,1])
+
+# 3차원 배열에서 1번째 행렬의 2번째 행의 4번째 열 필터링
+print(a3[0,1,2])
+
+
 
